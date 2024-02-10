cbacf7451744e2ad1e8844d84eee2237e3965f53
Golinsky <golinsky97@gmail.com>
2024-02-10 16:26:52 +0200

    - add effects for reinforcement

--- Assets/Scripts/Views/Reinforcement/ReinforcementView.cs
+++ Assets/Scripts/Views/Reinforcement/ReinforcementView.cs
@@ -1,4 +1,5 @@
-﻿using EmpireAtWar.Commands.Reinforcement;
+﻿using DG.Tweening;
+using EmpireAtWar.Commands.Reinforcement;
 using EmpireAtWar.Models.Factions;
 using EmpireAtWar.Models.Reinforcement;
 using EmpireAtWar.Views.ViewImpl;
@@ -14,10 +15,13 @@ namespace EmpireAtWar.Views.Reinforcement
         [SerializeField] private Button switchButton;
         [SerializeField] private Button closeButton;
         [SerializeField] private Canvas panelCanvas;
+        [SerializeField] private Image signalImage;
         
         private ReinforcementDraggable currentDraggable;
         private Transform spawnReinforcement;
-        
+        private Sequence fadeSequence;
+        private Color originColor;
+
         protected override void OnInitialize()
         {
             switchButton.onClick.AddListener(ActivateCanvas);
@@ -25,6 +29,7 @@ namespace EmpireAtWar.Views.Reinforcement
 
             Model.OnSpawnShip += HandleSpawning;
             Model.OnReinforcementAdded += AddUi;
+            originColor = signalImage.color;
         }
 
         protected override void OnDispose()
@@ -40,6 +45,15 @@ namespace EmpireAtWar.Views.Reinforcement
         {
             ReinforcementDraggable draggable = Instantiate(Model.ReinforcementButton, spawnTransform);
             draggable.Init(SpawnShip, shipType, sprite);
+            
+            if (fadeSequence != null && fadeSequence.IsActive())
+            {
+                fadeSequence.Kill();
+                fadeSequence.Append(signalImage.DOColor(originColor, 0.1f));
+            }
+            fadeSequence = DOTween.Sequence();
+            fadeSequence.Append(signalImage.DOColor(Color.green, 1f));
+            fadeSequence.Append(signalImage.DOColor(originColor, 1f));
         }
         
         private void HandleSpawning(bool success)
