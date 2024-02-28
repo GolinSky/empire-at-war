acb7b95e6fdbb7665cf2d14ed714338b7ebc857b
Golinsky <golinsky97@gmail.com>
2024-02-28 19:09:18 +0200

    - add settings template mvc

---
+++ Assets/Scripts/Views/Settings/SettingsView.cs
@@ -0,0 +1,18 @@
+﻿using EmpireAtWar.Commands.Settings;
+using EmpireAtWar.Models.Settings;
+using EmpireAtWar.Views.ViewImpl;
+
+namespace EmpireAtWar.Views.Settings
+{
+    public class SettingsView: View<ISettingsModelObserver, ISettingsCommand>
+    {
+        protected override void OnInitialize()
+        {
+            
+        }
+
+        protected override void OnDispose()
+        {
+        }
+    }
+}
