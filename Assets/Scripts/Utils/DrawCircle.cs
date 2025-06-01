eeea2c3b08f13381fdf47fd456e9e2fd8fc899c2
GollinSky <golinsky97@gmail.com>
2025-06-01 13:13:58 +0300

    - change borders line renderer mat and prefabs

--- Assets/Scripts/Utils/DrawCircle.cs
+++ Assets/Scripts/Utils/DrawCircle.cs
@@ -4,29 +4,28 @@ namespace EmpireAtWar
 {
     public class DrawCircle : MonoBehaviour
     {
-        public LineRenderer lineRenderer;
-        public int segments = 100; // Number of segments for smoothness
-        public float radius = 5f;
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private int segments = 100; // Number of segments for smoothness
+        [SerializeField] private float radius = 5f;
 
-        private float y;
-        void Start()
+        private float _y;
+        private void Start()
         {
-            y = transform.position.y;
-            lineRenderer = GetComponent<LineRenderer>();
-            lineRenderer.positionCount = segments + 1; // Add one to loop the circle
             CreatePoints();
         }
 
-        void CreatePoints()
+        private void CreatePoints()
         {
+            _y = transform.position.y;
+            lineRenderer = GetComponent<LineRenderer>();
+            lineRenderer.positionCount = segments + 1; 
             float angle = 0f;
             for (int i = 0; i < (segments + 1); i++)
             {
                 float x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
                 float z = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
 
-                // Set the position in world space, adjust Y to place it above your scene
-                lineRenderer.SetPosition(i, new Vector3(x, y, z));
+                lineRenderer.SetPosition(i, new Vector3(x, _y, z));
 
                 angle += (360f / segments);
             }
