81fa68625c81bed9d92674999b142526a9874e4d
GollinSky <golinsky97@gmail.com>
2026-07-12 17:59:19 +0300

    - fix move to target logic for player units

--- Assets/Scripts/Components/Commands/Ship/PlayerShipCommand.cs
+++ Assets/Scripts/Components/Commands/Ship/PlayerShipCommand.cs
@@ -1,17 +1,22 @@
+using EmpireAtWar.Entities.BaseEntity.EntityCommands;
 using EmpireAtWar.Ship;
 using EmpireAtWar.Mvc;
+using UnityEngine;
 
 namespace EmpireAtWar.Commands.Ship
 {
     public interface IShipCommand:ICommand
     {
     }
-    public class PlayerShipCommand: Command<EmpireAtWar.Ship.Ship>, IShipCommand
+    public class PlayerShipCommand: Command<EmpireAtWar.Ship.Ship>, IShipCommand, IMoveCommand
     {
         public PlayerShipCommand(EmpireAtWar.Ship.Ship ship) : base(ship)
         {
-    
         }
-        
+
+        public void MoveTo(Vector2 screenPosition)
+        {
+            Controller.MoveTo(screenPosition);
+        }
     }
 }
